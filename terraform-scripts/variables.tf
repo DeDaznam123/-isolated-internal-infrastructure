8c227337a5077654b0265fd896ea84c0a8d1713b
Victor_Handzhiev <50800277+DeDaznam123@users.noreply.github.com>
2024-05-24 14:40:21 +0300

    Update variables.tf

--- terraform-scripts/variables.tf
+++ terraform-scripts/variables.tf
@@ -5,3 +5,13 @@ variable "gitlab_instance_url" {
 variable "registration_token" {
   type = string
 }
+
+variable "rds_username" {
+  type = string
+  default = "victor"
+}
+
+variable "rds_password" {
+  type = string
+  default = "victor123"
+}
